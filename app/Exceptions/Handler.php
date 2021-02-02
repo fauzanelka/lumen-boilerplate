41df644eba01bf7f853c4afafdf8fd1c2c7f0b5c
Fauzan Elka <fauzan.elka@hotmail.com>
2021-02-03 01:21:09 +0700

    Implement standard errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,8 +4,11 @@ namespace App\Exceptions;
 
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
@@ -39,6 +42,25 @@ class Handler extends ExceptionHandler
     }
 
     /**
+     * Get status code from exception
+     *
+     * @param  \Throwable $exception
+     * @return int
+     */
+    private function getStatusCodeFromException(Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException) {
+            $flattenException = FlattenException::create($exception, 404);
+        } elseif ($exception instanceof AuthorizationException) {
+            $flattenException = FlattenException::create($exception, 403);
+        } else {
+            $flattenException = FlattenException::create($exception);
+        }
+
+        return $flattenException->getStatusCode();
+    }
+
+    /**
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -49,6 +71,20 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof HttpResponseException) {
+            return $exception->getResponse();
+        } elseif ($exception instanceof ValidationException && $exception->getResponse()) {
+            return $exception->getResponse();
+        }
+
+        $statusCode = $this->getStatusCodeFromException($exception);
+        $error['error'] = Response::$statusTexts[$statusCode];
+        if (env('APP_DEBUG', false)) {
+            $error['message'] = $exception->getMessage();
+            $error['file'] = $exception->getFile() . ':' . $exception->getLine();
+            $error['trace'] = explode('\n', $exception->getTraceAsString());
+        }
+
+        return response()->json($error, $statusCode, [], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
     }
 }
